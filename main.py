5be9653fbd5045311f0dced874977237f3df13fc
Vinicius Silva <74320425+typevs@users.noreply.github.com>
2021-11-06 10:48:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-import os, time, random, sys
-
-cmd = os.system
-
-carac = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-
+import random, string
 
 
 def main():
     try:
-        cmd('cls')
         print(f'Put how many characters you want to generate (If you put 0 or nothing, it will come with 8)')
         amount = input('> ')
-        cmd('cls')
     
         if amount == '0':
             amount = '8'
 
         if amount == '':
             amount = '8'
+
         print(f'\nPassword Generated:\n')
         try:
             for i in range(int(amount)):
-           
-                password = print(random.choice(carac), end="")
+        
+                senha = print(random.choice(string.digits + string.ascii_letters), end="")
             print(f'\n')
         except:
             main()
     except KeyboardInterrupt:
         exit()
-  
-        
 
 
 
